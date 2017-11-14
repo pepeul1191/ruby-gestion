de6825088ae0bb01dd9eb61c4ddf9b5fbe4246f5
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-14 11:22:45 -0500

    Listar permisos del usuario

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get 'accesos/usuario/listar_sistemas/:usuario_id' => 'accesos/sistema#usuario'
   get 'accesos/usuario/logs/:usuario_id' => 'accesos/usuario#logs'
   get 'accesos/usuario/listar_roles/:sistema_id/:usuario_id' => 'accesos/usuario#listar_roles'
+  get 'accesos/usuario/listar_permisos/:sistema_id/:usuario_id' => 'accesos/usuario#listar_permisos'
   get 'accesos/usuario/obtener_usuario_correo/:usuario_id' => 'accesos/usuario#obtener_usuario_correo'
 end
