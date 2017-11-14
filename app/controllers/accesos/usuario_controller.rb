307f29deb636f426ef5d441776b778759d7db09a
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-14 11:21:28 -0500

    Listar rolres del usuario

--- app/controllers/accesos/usuario_controller.rb
+++ app/controllers/accesos/usuario_controller.rb
@@ -12,4 +12,19 @@ class Accesos::UsuarioController < ApplicationController
 	def obtener_usuario_correo
 		render :plain => Accesos::Usuario.select(:usuario, :correo).where(:id => params[:usuario_id]).first.to_json
 	end
+	#listar_roles/:sistema_id/:usuario_id
+	def listar_roles
+		render :plain => DB_ACCESOS.fetch('
+			SELECT T.id AS id, T.nombre AS nombre, (CASE WHEN (P.existe = 1) THEN 1 ELSE 0 END) AS existe FROM
+			(
+				SELECT id, nombre, 0 AS existe FROM roles WHERE sistema_id = ' + params[:sistema_id] + '
+			) T
+			LEFT JOIN
+			(
+				SELECT R.id, R.nombre, 1 AS existe  FROM roles R 
+				INNER JOIN usuarios_roles UR ON R.id = UR.rol_id
+				WHERE UR.usuario_id = ' + params[:usuario_id] + '
+			) P
+			ON T.id = P.id').to_a.to_json
+	end
 end
