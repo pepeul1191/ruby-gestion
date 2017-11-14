f4e9d17fd4241661c4890c12f9f78974efbad182
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-14 10:55:48 -0500

    Obtener usuario y correo

--- app/controllers/accesos/usuario_controller.rb
+++ app/controllers/accesos/usuario_controller.rb
@@ -1,7 +1,15 @@
 class Accesos::UsuarioController < ApplicationController
-    def listar
+	def listar
 		render :plain => DB_ACCESOS.fetch('
-        	SELECT U.id AS id, U.usuario AS usuario, A.momento AS momento, U.correo AS correo 
-        	FROM usuarios U INNER JOIN accesos A ON U.id = A.usuario_id GROUP BY U.usuario ORDER BY U.id').to_a.to_json
+			SELECT U.id AS id, U.usuario AS usuario, A.momento AS momento, U.correo AS correo 
+			FROM usuarios U INNER JOIN accesos A ON U.id = A.usuario_id GROUP BY U.usuario ORDER BY U.id').to_a.to_json
+	end
+
+	def logs
+		render :plain => Accesos::Acceso.select(:id, :momento).where(:usuario_id => params[:usuario_id]).to_a.to_json
+	end
+
+	def obtener_usuario_correo
+		render :plain => Accesos::Usuario.select(:usuario, :correo).where(:id => params[:usuario_id]).first.to_json
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'accesos' => 'accesos/view#index'
   get 'accesos/sistema/listar' => 'accesos/sistema#listar'
-  get 'accesos/usuario/listar' => 'accesos/usuario#listar'
+  get 'accesos/usuario/obtener_usuario_correo/:usuario_id' => 'accesos/usuario#obtener_usuario_correo'
+  get 'accesos/usuario/logs/:usuario_id' => 'accesos/usuario#logs'
 end
