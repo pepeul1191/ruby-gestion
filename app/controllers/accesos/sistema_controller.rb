d884c516d1e3b3a348f7b1838d6b53e46a830c33
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-15 22:28:18 -0500

    Listar sistemas del usuario

--- app/controllers/accesos/sistema_controller.rb
+++ app/controllers/accesos/sistema_controller.rb
@@ -1,23 +1,11 @@
 class Accesos::SistemaController < ApplicationController
 	#protect_from_forgery except: :listar
-
 	def listar
 		render :plain => get(CONSTANTS[:servicios][:accesos] + 'sistema/listar')
 	end
 
 	def usuario
-		render :plain => DB_ACCESOS.fetch('
-			SELECT T.id AS id, T.nombre AS nombre, (CASE WHEN (P.existe = 1) THEN 1 ELSE 0 END) AS existe FROM
-			(
-				SELECT id, nombre, 0 AS existe FROM sistemas
-			) T
-			LEFT JOIN
-			(
-				SELECT S.id, S.nombre, 1 AS existe FROM sistemas S
-				INNER JOIN usuarios_sistemas US ON US.sistema_id = S.id
-				WHERE US.usuario_id = ' + params[:usuario_id] + '
-			) P
-			ON T.id = P.id').to_a.to_json
+			render :plain => get(CONSTANTS[:servicios][:accesos] + 'sistema/usuario/' + params[:usuario_id])
 	end
 
 	def guardar
