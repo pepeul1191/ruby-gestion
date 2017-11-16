e09c3e641d62c15d473334c9653898de4da502c1
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-15 22:22:51 -0500

    CRUD de sistemas

--- app/controllers/accesos/sistema_controller.rb
+++ app/controllers/accesos/sistema_controller.rb
@@ -21,49 +21,6 @@ class Accesos::SistemaController < ApplicationController
 	end
 
 	def guardar
-	  data = JSON.parse(params[:data])
-	  nuevos = data['nuevos']
- 	  editados = data['editados']
-	  eliminados = data['eliminados']
-	  usuario_id = data['extra']
-		rpta = [] 
-		array_nuevos = []
-		error = false
-		execption = nil
-	  DB_ACCESOS.transaction do
-			begin  
-				if nuevos.length != 0
-					nuevos.each do |nuevo|
-						n = Accesos::Sistema.new(:nombre => nuevo['nombre'], :version => nuevo['version'], :repositorio => nuevo['repositorio'])
-						n.save
-						t = {:temporal => nuevo['id'], :nuevo_id => n.id}
-						array_nuevos.push(t)
-					end
-				end
-				if editados.length != 0
-					editados.each do |editado|
-						e = Accesos::Sistema.where(:id => editado['id']).first
-						e.nombre = editado['nombre']
-						e.version = editado['version']
-						e.repositorio = editado['repositorio']
-						e.save
-					end
-				end
-				if eliminados.length != 0
-					eliminados.each do |eliminado|
-						Accesos::Sistema.where(:id => eliminado).delete
-					end
-				end
-			rescue Exception => e
-				Sequel::Rollback
-				error = true
-				execption = e	
-			end
-	  end
-		if error == false
-			render :plain => {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los sistemas', array_nuevos]}.to_json
-		else
-			render :plain => {:tipo_mensaje => 'error', :mensaje => ['Se ha producido un error en guardar la tabla de sistemas', execption.message]}.to_json, status: 500
-		end
+			render :plain => post(CONSTANTS[:servicios][:accesos] + 'sistema/guardar?data=' + params[:data])
 	end
 end
