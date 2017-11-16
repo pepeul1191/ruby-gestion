c8fdf09b821ec3f1d50270883fe5aacfe853b27b
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-16 00:19:58 -0500

    Asociar y desasociar permisos a rol

--- app/controllers/accesos/permiso_controller.rb
+++ app/controllers/accesos/permiso_controller.rb
@@ -4,18 +4,7 @@ class Accesos::PermisoController < ApplicationController
 	end
 
 	def listar_asociados
-		render :plain => DB_ACCESOS.fetch('
-		SELECT T.id AS id, T.nombre AS nombre, (CASE WHEN (P.existe = 1) THEN 1 ELSE 0 END) AS existe, T.llave AS llave FROM 
-		(
-			SELECT id, nombre, llave, 0 AS existe FROM permisos WHERE sistema_id = ' + params[:sistema_id] + ' 
-		) T
-		LEFT JOIN
-		(
-			SELECT P.id, P.nombre,  P.llave, 1 AS existe  FROM permisos P 
-			INNER JOIN roles_permisos RP ON P.id = RP.permiso_id
-			WHERE RP.rol_id =  ' + params[:rol_id] + ' 
-		) P
-		ON T.id = P.id').to_a.to_json
+		render :plain => get(CONSTANTS[:servicios][:accesos] + 'permiso/listar_asociados/' + params[:sistema_id] + '/' + params[:rol_id])
 	end
 
 	def guardar
--- app/controllers/accesos/rol_controller.rb
+++ app/controllers/accesos/rol_controller.rb
@@ -8,31 +8,6 @@ class Accesos::RolController < ApplicationController
 	end
 
 	def asociar_permisos
-		data = JSON.parse(params[:data])
-	  nuevos = data['nuevos']
- 	  editados = data['editados']
-	  eliminados = data['eliminados']
-	  usuario_id = data['extra']
-	  rol_id = data['extra']['id_rol']
-	  rpta = [] 
-	  DB_ACCESOS.transaction do
-			begin  
-				if nuevos.length != 0
-					nuevos.each do |nuevo|
-						n = Accesos::RolPermiso.new(:permiso_id => nuevo['id'], :rol_id => rol_id)
-						n.save
-					end
-				end
-				if eliminados.length != 0
-					eliminados.each do |eliminado|
-						Accesos::RolPermiso.where(:permiso_id => eliminado, :rol_id => rol_id).delete
-					end
-				end
-			rescue Exception => e
-				raise Sequel::Rollback  
-				render :plain => {:tipo_mensaje => 'error', :mensaje => ['Se ha registrado la asociación/deasociación de los permisos al rol', e.message]}.to_json, status: 500
-			end
-	  end
-		render :plain => {:tipo_mensaje => 'success', :mensaje => ['Se ha registrado los cambios en los roles', []]}.to_json
+		render :plain => post(CONSTANTS[:servicios][:accesos] + 'rol/asociar_permisos?data=' + params[:data])
 	end
 end
