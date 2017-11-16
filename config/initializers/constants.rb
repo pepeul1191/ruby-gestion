59feb0c3128e17a01e3654ffdef1d5e8657173da
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-16 11:36:50 -0500

    UML de arquitectura agregada

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,6 +3,7 @@ CONSTANTS = {
     :STATIC_URL => 'http://localhost:3000/',
     :CSRF => 'mpt/sr6eS2AlCRHU7DVThMgFTN08pnfSDf/C94eZx7udfm0lvgaYWLYJttYPKzGKDTlXwVU/d2FOxbKkgNlsTw==',
     :ambiente => 'desarrollo',
+    :key => 'Fdp6CVxpri1ga8kH',
     :servicios => {
         :accesos => 'http://localhost:5000/'
     }
